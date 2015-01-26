13cea92113d56691a6f47b03670f117f8706fc97
ci-one <seongyu_kim@ci-one.com>
2015-01-26 10:26:54 +0900

    Initial commit

---
+++ 1820EN_04_Code/07 - event handlers/eventHandlers.js
@@ -0,0 +1,11 @@
+angular.module('eventHandlers', [])
+
+  .controller('EventhandlersCtrl', function ($scope) {
+
+    $scope.items = ['foo', 'bar', 'baz'];
+
+    $scope.logPosition = function (item, $event) {
+      console.log($event)
+      /*console.log(item + ' was clicked at: ' + $event.clientX + ',' + $event.clientY);*/
+    };
+  });
